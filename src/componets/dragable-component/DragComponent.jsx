eb338a32a7bb86fadceba381fd23801a546c99bf
Tomas Kravcik <root@Laptop-i7>
2024-11-19 09:37:15 +0100

    basic draggable react component - add bounds and handler

--- src/componets/dragable-component/DragComponent.jsx
+++ src/componets/dragable-component/DragComponent.jsx
@@ -1,22 +1,37 @@
-import Draggable, {DraggableCore} from 'react-draggable';
+import Draggable from 'react-draggable';
 import {useState} from "react";
 
 export const DragComponent = () => {
     const [position, setPosition] = useState({x: 0, y: 0})
     const handleDrag = (e, position) => {
-       setPosition({
-           x: position.x,
-           y: position.y
-       })
+        setPosition({
+            x: position.x,
+            y: position.y
+        })
     }
-    return (
-        <Draggable onDrag={handleDrag}>
-            <div style={{
-                backgroundColor: 'yellow',
-                width: '100px',
-                height: '50px'
-            }}
-                 className={"handle"}>DRAG on position {position.x + " " + position.y}</div>
-        </Draggable>
+    return (<>
+            <Draggable onDrag={handleDrag} bounds={'parent'}>
+                <div style={{
+                    backgroundColor: 'blue',
+                    width: '100px',
+                    height: '50px'
+                }}
+                     className={"handle"}>DRAG on position X:{position.x} Y:{position.y}</div>
+            </Draggable>
+
+
+            <Draggable onDrag={handleDrag} >
+                <div className={'handle'}>
+                    HADLE BAR
+
+                    <div style={{
+                        backgroundColor: 'yellow',
+                        width: '100px',
+                        height: '50px'
+                    }}
+                    >DRAG on position {position.x + " " + position.y}</div>
+                </div>
+            </Draggable>
+        </>
     )
 }
